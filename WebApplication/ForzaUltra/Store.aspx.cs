2ec11ae392ccce0c796d0989ce3dbb6c2dbac1dc
OzcarHdez <oscar.hernandez@vantis.mx>
2020-05-20 08:59:51 -0500

    Funcionalidad de agregar items al carrito

--- WebApplication/ForzaUltra/Store.aspx.cs
+++ WebApplication/ForzaUltra/Store.aspx.cs
@@ -25,7 +25,74 @@ namespace WebApplication.ForzaUltra
         [WebMethod]
         public static ResponseListDTO<ProductCategoryDTO> GetStoreGetList()
         {
+            HttpContext.Current.Session["SessionInit"] = false;
             var response = new ProductCategoryLogic().CategoryListForMainPage(4);
+            return response;
+        }
+
+        [WebMethod]
+        public static ResponseDTO<CartDTO> CartItemExecute(CartDTO item)
+        {
+            item.Customer = new CustomerDTO
+            {
+                Identifier = 1
+            };
+
+            var request = new RequestDTO<CartDTO>
+            {
+                Item = item,
+                OperationType = OperationType.Insert
+            };
+
+
+            if (HttpContext.Current.Session["CURRENT_CART_GUID"] != null)
+            {
+                var list = new CartLogic().CartGetFilteredList(new RequestDTO<CartDTO>
+                {
+                    Item = new CartDTO
+                    {
+                        Identifier = item.Identifier,
+                        Customer = item.Customer
+                    }
+                });
+
+                if (list.Success)
+                {
+                    var exist = list.Result.FirstOrDefault(x => x.ProductCatalog.Identifier == item.ProductCatalog.Identifier && x.ProductCategory.Identifier == item.ProductCategory.Identifier);
+                    if (exist != null)
+                    {
+                        request.OperationType = OperationType.Update;
+                        request.Item.Quantity += exist.Quantity;
+                    }
+                }
+
+                request.Item.Identifier = HttpContext.Current.Session["CURRENT_CART_GUID"].ToString();
+            }
+
+            var response = new CartLogic().CartItemExecute(request);
+
+            if (response.Success)
+            {
+                HttpContext.Current.Session["CURRENT_CART_GUID"] = response.Result.Identifier;
+            }
+
+            return response;
+        }
+
+        [WebMethod]
+        public static ResponseListDTO<CartDTO> CartGetFilteredList()
+        {
+            var response = new CartLogic().CartGetFilteredList(new RequestDTO<CartDTO>
+            {
+                Item = new CartDTO
+                {
+                    Customer = new CustomerDTO
+                    {
+                        Identifier = 1
+                    },
+                    Identifier = HttpContext.Current.Session["CURRENT_CART_GUID"].ToString() ?? string.Empty
+                }
+            });
             return response;
         }
     }
