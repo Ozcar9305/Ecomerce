f716109bba71f4fd3427c2abbeac77318df74cfb
jorge gallegos <jgallegosledon@hotmail.com>
2020-05-25 23:37:33 -0500

    Se cambia nombre de pagina default

--- WebApplication/Default.aspx.cs
+++ WebApplication/Default.aspx.cs
@@ -11,7 +11,7 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication
 {
-    public partial class _Default : Page
+    public partial class Default : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
